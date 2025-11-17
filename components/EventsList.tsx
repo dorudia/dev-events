7091c147441c8b715ccda546a373fecbd9e1e2f8
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-11-17 11:35:10 +0200

    Load real events.

--- components/EventsList.tsx
+++ components/EventsList.tsx
@@ -1,17 +1,17 @@
 import EventCard from "@/components/EventCard";
 import { IEvent } from "@/models/Event";
-import { events } from "@/lib/constants";
+// import { events } from "@/lib/constants";
 
 const EventsList = async () => {
-  // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/event`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/event`);
 
-  // if (!res.ok) {
-  //   const text = await res.text();
-  //   console.error("Failed to fetch events:", text);
-  //   return <p>Failed to load events.</p>;
-  // }
+  if (!res.ok) {
+    const text = await res.text();
+    console.error("Failed to fetch events:", text);
+    return <p>Failed to load events.</p>;
+  }
 
-  // const events = await res.json();
+  const events = await res.json();
 
   return (
     <>
