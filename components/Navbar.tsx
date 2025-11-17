1505299701d652af45c5c6c51510e60deedb40c8
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-11-17 10:02:40 +0200

    Implement Prod routes.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,16 +5,20 @@ const Navbar = () => {
   return (
     <header>
       <nav>
-        <Link href="/" className="flex">
+        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}`} className="flex">
           <Image src="/icons/logo.png" alt="Logo" width={24} height={24} />
           <p>DevEvent</p>
         </Link>
         <ul>
-          <Link href="/">Home</Link>
+          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}`}>Home</Link>
 
-          <Link href="/about">Events</Link>
+          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/events`}>
+            Events
+          </Link>
 
-          <Link href="/contact">Create Event</Link>
+          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/create`}>
+            Create Event
+          </Link>
         </ul>
       </nav>
     </header>
