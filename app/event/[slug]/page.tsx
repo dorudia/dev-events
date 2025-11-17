0a6c6912f637ad3e2658bee749224254e8a4fc22
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-11-17 08:47:15 +0200

    Added Event page.

--- app/event/[slug]/page.tsx
+++ app/event/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import BookEvent from "@/components/BookEvent";
+import Event from "@/components/Event";
 import EventCard from "@/components/EventCard";
 import SimilarEventsList from "@/components/SimilarEventsList";
 import { getBookingsByEmail } from "@/lib/actions/booking.actions";
@@ -18,95 +19,18 @@ interface PageProps {
   };
 }
 
-const EventDetailItem = ({
-  icon,
-  alt,
-  label,
-}: {
-  icon: string;
-  alt: string;
-  label: string;
-}) => (
-  <div className="flex flex-row gap-2 items-center mb-2">
-    <Image
-      src={icon}
-      alt={alt}
-      width={20}
-      height={20}
-      style={{ width: "auto", height: "auto" }}
-    />
-    <p>{label}</p>
-  </div>
-);
-
 const EventDetaisPage = async ({ params }: PageProps) => {
   "use cache";
-  cacheLife({ revalidate: 60 }); // Revalidate every 60 seconds
+  cacheLife({ revalidate: 60 });
+
   const { slug } = await params;
-  const data = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/event/${slug},  { cache: "no-store" } `
-  );
 
-  const { event } = await data.json();
-
-  if (!event) return notFound();
+  if (!slug) return notFound(); // fallback dacă nu există slug
 
   return (
-    <section id="event">
-      <h1>{event.title}</h1>
-      <div className="header">
-        {event.description && (
-          <>
-            <h1>Event Descripton</h1>
-            <p className="mt-2">{event.description}</p>
-          </>
-        )}
-      </div>
-      <div className="details">
-        <div className="content">
-          <Image
-            className="banner"
-            src={event.image}
-            alt="event poster"
-            width={800}
-            height={800}
-          />
-          <section className="flex-col-gap-2">
-            <h2>Event Details</h2>
-            <EventDetailItem
-              icon="/icons/calendar.svg"
-              alt="calendar"
-              label={event.date}
-            />
-            <EventDetailItem
-              icon="/icons/clock.svg"
-              alt="clock"
-              label={event.time}
-            />
-            <EventDetailItem
-              icon="/icons/pin.svg"
-              alt="location"
-              label={event.location}
-            />
-          </section>
-        </div>
-        <aside className="booking">
-          <p className="text-lg font-semibold mb-4">Book Event</p>
-          <div className="signup-card">
-            <h2>Book your spot</h2>
-            <BookEvent eventId={event._id} slug={event.slug} />
-          </div>
-        </aside>
-      </div>
-      <div className="flex w-full flex-col gap-4 pt-20">
-        <h2>Similar Events</h2>
-        <ul className="list-none flex flex-direction-row flex-wrap gap-4 mb-4">
-          <Suspense fallback={<div>Loading similar events...</div>}>
-            <SimilarEventsList slug={slug} />
-          </Suspense>
-        </ul>
-      </div>
-    </section>
+    <Suspense fallback={<div>Loading event details...</div>}>
+      <Event slug={slug} /> {/* propagat corect */}
+    </Suspense>
   );
 };
 
---
+++ components/Event.tsx
@@ -0,0 +1,94 @@
+import Image from "next/image";
+import React, { Suspense } from "react";
+import BookEvent from "./BookEvent";
+import SimilarEventsList from "./SimilarEventsList";
+import { notFound } from "next/navigation";
+
+const EventDetailItem = ({
+  icon,
+  alt,
+  label,
+}: {
+  icon: string;
+  alt: string;
+  label: string;
+}) => (
+  <div className="flex flex-row gap-2 items-center mb-2">
+    <Image
+      src={icon}
+      alt={alt}
+      width={20}
+      height={20}
+      style={{ width: "auto", height: "auto" }}
+    />
+    <p>{label}</p>
+  </div>
+);
+
+const Event = async ({ slug }: { slug: string }) => {
+  const data = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/event/${slug}`
+  );
+  const { event } = await data.json();
+  if (!event) return notFound();
+
+  return (
+    <section id="event">
+      <h1>{event.title}</h1>
+      <div className="header">
+        {event.description && (
+          <>
+            <h1>Event Descripton</h1>
+            <p className="mt-2">{event.description}</p>
+          </>
+        )}
+      </div>
+      <div className="details">
+        <div className="content">
+          <Image
+            className="banner"
+            src={event.image}
+            alt="event poster"
+            width={800}
+            height={800}
+          />
+          <section className="flex-col-gap-2">
+            <h2>Event Details</h2>
+            <EventDetailItem
+              icon="/icons/calendar.svg"
+              alt="calendar"
+              label={event.date}
+            />
+            <EventDetailItem
+              icon="/icons/clock.svg"
+              alt="clock"
+              label={event.time}
+            />
+            <EventDetailItem
+              icon="/icons/pin.svg"
+              alt="location"
+              label={event.location}
+            />
+          </section>
+        </div>
+        <aside className="booking">
+          <p className="text-lg font-semibold mb-4">Book Event</p>
+          <div className="signup-card">
+            <h2>Book your spot</h2>
+            <BookEvent eventId={event._id} slug={event.slug} />
+          </div>
+        </aside>
+      </div>
+      <div className="flex w-full flex-col gap-4 pt-20">
+        <h2>Similar Events</h2>
+        <ul className="list-none flex flex-direction-row flex-wrap gap-4 mb-4">
+          <Suspense fallback={<div>Loading similar events...</div>}>
+            <SimilarEventsList slug={slug} />
+          </Suspense>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Event;
