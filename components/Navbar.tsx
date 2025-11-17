b60c76db571c22b3235903491621fde0a739b059
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-11-17 12:04:51 +0200

    Production setup 2.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,20 +5,16 @@ const Navbar = () => {
   return (
     <header>
       <nav>
-        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}`} className="flex">
+        <Link href="/" className="flex">
           <Image src="/icons/logo.png" alt="Logo" width={24} height={24} />
           <p>DevEvent</p>
         </Link>
         <ul>
-          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}`}>Home</Link>
+          <Link href="/">Home</Link>
 
-          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/events`}>
-            Events
-          </Link>
+          <Link href="/events">Events</Link>
 
-          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/create`}>
-            Create Event
-          </Link>
+          <Link href="/events/create">Create Event</Link>
         </ul>
       </nav>
     </header>
