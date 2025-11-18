85f2d98e910b0d271d02f9bd93321c78e7f5d267
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-11-18 09:44:21 +0200

    Implemrnt Clerk auth.

--- app/event/create/page.tsx
+++ app/event/create/page.tsx
@@ -1,115 +1,23 @@
-// "use client";
-
-// import React from "react";
-// import { useRouter } from "next/navigation";
-
-// const CreateEvent = () => {
-//   const router = useRouter();
-
-//   const submitHandler = async (e: React.FormEvent) => {
-//     e.preventDefault();
-//     const form = e.currentTarget as HTMLFormElement;
-//     const formData = new FormData(form);
-//     const data = Object.fromEntries(formData);
-//     console.log("FORM DATA:", data);
-
-//     const res = await fetch("/api/event", {
-//       method: "POST",
-//       body: formData, // important
-//     });
-//     const result = await res.json();
-//     console.log("RESULT:", result);
-//     if (res.ok) {
-//       // 1️⃣ reset input-urile
-//       form.reset();
-
-//       // 2️⃣ redirect la /events
-//       router.push("/events");
-//     } else {
-//       // eventual afișezi eroare
-//       alert(result.error || "Failed to create event");
-//     }
-//   };
-
-//   return (
-//     <div className="border border-amber-50 p-4 max-w-2xl mx-auto mt-3 rounded-md">
-//       <h3 className="text-center">Create Event</h3>
-//       <form onSubmit={submitHandler}>
-//         <label htmlFor="title">Event Title</label>
-//         <input
-//           type="text"
-//           id="title"
-//           name="title"
-//           required
-//           className="w-full bg-gray-800 rounded-md p-3 mb-4 active:border-none focus:border-none focus:outline-none"
-//           placeholder="Enter event title"
-//         />
-
-//         <label htmlFor="slug">Event Slug</label>
-//         <input
-//           type="text"
-//           id="slug"
-//           name="slug"
-//           required
-//           className="w-full bg-gray-800 rounded-md p-3 mb-4 active:border-none focus:border-none focus:outline-none"
-//           placeholder="Enter event slug"
-//         />
-
-//         <label htmlFor="image">Select Event Image</label>
-//         <input
-//           type="file"
-//           id="image"
-//           name="image"
-//           required
-//           className="w-full bg-gray-800 rounded-md p-3 mb-4 active:border-none focus:border-none focus:outline-none"
-//           placeholder="Enter event image URL"
-//         />
-//         <label htmlFor="location">Event Location</label>
-//         <input
-//           type="text"
-//           id="location"
-//           name="location"
-//           required
-//           className="w-full bg-gray-800 rounded-md p-3 mb-4 active:border-none focus:border-none focus:outline-none"
-//           placeholder="Enter event location"
-//         />
-//         <label htmlFor="date">Event Date</label>
-//         <input
-//           type="date"
-//           id="date"
-//           name="date"
-//           required
-//           className="w-full bg-gray-800 rounded-md p-3 mb-4 active:border-none focus:border-none focus:outline-none"
-//         />
-//         <label htmlFor="time">Event Time</label>
-//         <input
-//           type="time"
-//           id="time"
-//           name="time"
-//           required
-//           className="w-full bg-gray-800 rounded-md p-3 mb-4 active:border-none focus:border-none focus:outline-none"
-//         />
-
-//         <button
-//           type="submit"
-//           className="bg-amber-600 px-4 py-2 rounded-md mt-2 w-full"
-//         >
-//           Create Event
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default CreateEvent;
-
 "use client";
 
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@clerk/nextjs";
 
 const CreateEvent = () => {
+  const { isSignedIn } = useAuth();
   const router = useRouter();
+
+  if (!isSignedIn) {
+    return (
+      <div className="text-center mt-10">
+        <h2 className="text-2xl font-semibold mb-4 text-amber-500">
+          Please Sign In to create an event.
+        </h2>
+      </div>
+    );
+  }
+
   const [submitting, setSubmitting] = useState(false);
 
   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
