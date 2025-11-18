e57c1f4d839662c9b6e7bd9da35a708358e2ba4c
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-11-18 09:54:42 +0200

    Implemrnt Page Not Found.

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <h2 className="text-center">404</h2>
+      <h1 className="text-center">Page Not Found</h1>
+    </>
+  );
+};
+
+export default NotFound;
